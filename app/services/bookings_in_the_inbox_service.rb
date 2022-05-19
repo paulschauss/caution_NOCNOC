8ad441d5b67168c3d6773a5c7cb66d2326a9dc96
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-05-19 19:53:31 +0200

    service des bookings qui pete parce que trop

--- app/services/bookings_in_the_inbox_service.rb
+++ app/services/bookings_in_the_inbox_service.rb
@@ -5,12 +5,20 @@ require 'openssl'
 class BookingsInTheInboxService
 
   def call
+    if Rails.env.development?
+      Booking.destroy_all
+      Guest.destroy_all
+    end
     set_url
     set_bookings
+    create_bookings
+    # test_method
   end
 
+  private
+
   def set_url
-    @url = URI("https://api.lodgify.com/v1/reservation")
+    @url = URI("https://api.lodgify.com/v1/reservation?offset=0&limit=50&trash=false")
   end
 
   def set_bookings
@@ -22,7 +30,56 @@ class BookingsInTheInboxService
     request["X-ApiKey"] = ENV["LODGIFY_API_KEY"]
 
     response = http.request(request)
-    @bookings = JSON.parse(response.read_body)['items']
-    ap @booking
+    @api_bookings = JSON.parse(response.read_body)['items']
+  end
+
+  def create_bookings
+    @api_bookings.each do |api_booking|
+      if Booking.where(lodgify_id: api_booking['id']).empty?
+
+        ## Set the Guest
+        api_guest = api_booking['guest']
+
+        # Create the Guest
+        guest = create_guest(api_guest)
+
+        if Property.where(lodgify_id: api_booking['property_id']).any?
+
+          ## Set the Property
+          property = Property.find_by(lodgify_id: api_booking['property_id'])
+
+          # Create the Booking
+          Booking.create!(
+            lodgify_id: api_booking['id'],
+            property: property,
+            guest: guest,
+            arrival: api_booking['arrival'],
+            departure: api_booking['departure']
+          )
+
+        else
+
+          ap "Property doesn't exists"
+
+        end
+      end
+    end
+
+    return "C'est fini !"
+
+  end
+
+  def create_guest(api_guest)
+    guest = Guest.create!(
+      lodgify_id: api_guest['id'],
+      name: api_guest['name'],
+      email: api_guest['email'],
+      phone: api_guest['phone'],
+    )
+    return guest
+  end
+
+  def test_method
+    ap @api_bookings
   end
 end
