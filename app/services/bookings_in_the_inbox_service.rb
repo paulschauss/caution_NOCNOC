d6ae442f93d25aeb7b2954b69e045f370678521a
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-05-20 10:23:29 +0200

    changed the booking service, call it for each property

--- app/services/bookings_in_the_inbox_service.rb
+++ app/services/bookings_in_the_inbox_service.rb
@@ -9,32 +9,38 @@ class BookingsInTheInboxService
       Booking.destroy_all
       Guest.destroy_all
     end
-    set_url
-    set_bookings
-    create_bookings
-    # test_method
+
+    Property.all.each do |property|
+      url = set_url(property)
+      api_bookings = set_bookings(url)
+      create_bookings(api_bookings, property)
+    end
   end
 
   private
 
-  def set_url
-    @url = URI("https://api.lodgify.com/v1/reservation?offset=0&limit=50&trash=false")
+  def set_url(property)
+    ap "je set l'url"
+    return URI("https://api.lodgify.com/v1/reservation?offset=0&limit=50&status=Booked&trash=false&propertyId=#{property.lodgify_id}")
   end
 
-  def set_bookings
-    http = Net::HTTP.new(@url.host, @url.port)
+  def set_bookings(url)
+    ap "je set les bookings"
+
+    http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
 
-    request = Net::HTTP::Get.new(@url)
+    request = Net::HTTP::Get.new(url)
     request["Accept"] = 'text/plain'
     request["X-ApiKey"] = ENV["LODGIFY_API_KEY"]
 
     response = http.request(request)
-    @api_bookings = JSON.parse(response.read_body)['items']
+    return JSON.parse(response.read_body)['items']
   end
 
-  def create_bookings
-    @api_bookings.each do |api_booking|
+  def create_bookings(api_bookings, property)
+    ap "je crée les bookings"
+    api_bookings.each do |api_booking|
       if Booking.where(lodgify_id: api_booking['id']).empty?
 
         ## Set the Guest
@@ -43,25 +49,14 @@ class BookingsInTheInboxService
         # Create the Guest
         guest = create_guest(api_guest)
 
-        if Property.where(lodgify_id: api_booking['property_id']).any?
-
-          ## Set the Property
-          property = Property.find_by(lodgify_id: api_booking['property_id'])
-
-          # Create the Booking
-          Booking.create!(
-            lodgify_id: api_booking['id'],
-            property: property,
-            guest: guest,
-            arrival: api_booking['arrival'],
-            departure: api_booking['departure']
-          )
-
-        else
-
-          ap "Property doesn't exists"
-
-        end
+        # Create the Booking
+        Booking.create!(
+          lodgify_id: api_booking['id'],
+          property: property,
+          guest: guest,
+          arrival: api_booking['arrival'],
+          departure: api_booking['departure']
+        )
       end
     end
 
@@ -77,9 +72,5 @@ class BookingsInTheInboxService
       phone: api_guest['phone'],
     )
     return guest
-  end
-
-  def test_method
-    ap @api_bookings
   end
 end
