438c63dcb4d3735ab65424c065c73e4046d72725
paul schauss <p+github@schauss.fr>
2022-05-20 15:53:21 +0200

    property info list service to v2

--- app/services/properties_info_list_service.rb
+++ app/services/properties_info_list_service.rb
@@ -3,7 +3,6 @@ require 'net/http'
 require 'openssl'
 
 class PropertiesInfoListService
-
   def call
     Property.destroy_all if Rails.env.development?
     set_url
@@ -14,7 +13,7 @@ class PropertiesInfoListService
   private
 
   def set_url
-    @url = URI("https://api.lodgify.com/v1/properties")
+    @url = URI("https://api.lodgify.com/v2/properties?includeCount=true&includeInOut=false&page=1&size=5000")
   end
 
   def set_properties
@@ -23,15 +22,14 @@ class PropertiesInfoListService
 
     request = Net::HTTP::Get.new(@url)
     request["Accept"] = 'text/plain'
-    request["X-ApiKey"] = ENV["LODGIFY_API_KEY"]
+    request["X-ApiKey"] = ENV.fetch("LODGIFY_API_KEY") # ENV["LODGIFY_API_KEY"]
 
     response = http.request(request)
-    @api_properties = JSON.parse(response.read_body)
+    @api_properties = JSON.parse(response.read_body)['items']
   end
 
   def create_properties
     @api_properties.each do |api_property|
-
       ## set property fields
       property_lodgify_id = api_property["id"]
       property_name = api_property["name"]
