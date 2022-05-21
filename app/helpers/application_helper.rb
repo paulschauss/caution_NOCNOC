0e437e76e168f3f9be062b2c7ac0e00be605f09a
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-05-21 16:46:50 +0200

    method pour recuperer le montant d'une cuation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  ## recuperer le montant de la caution
+  def get_amount(caution)
+    caution.split(' ').filter { |word| word.chars.include?("€") }.to_s.chars.filter { |c| c == c.to_i.to_s }.join.to_i
+  end
 end
