5cc8c364d11df47ac24cfd96e201ecfc229a1021
paul schauss <p+github@schauss.fr>
2022-05-25 08:46:22 +0200

    working on turning all bookings API to v2

--- app/services/bookings_in_the_inbox_service.rb
+++ app/services/bookings_in_the_inbox_service.rb
@@ -3,6 +3,10 @@ require 'net/http'
 require 'openssl'
 
 class BookingsInTheInboxService
+  def initialize
+    @current_page_number = 1
+  end
+
   def call
     ## Reset the Bookings and the Guests in the Database
     PropertiesInfoListService.new.call
@@ -12,39 +16,50 @@ class BookingsInTheInboxService
     end
 
     ## Create the bookings for each property
-    Property.all.each do |property|
-      url = set_url(property)
-      api_bookings = set_bookings(url)
-      create_bookings(api_bookings, property)
-    end
-
+    set_url
+    set_json
+    set_page_number
+    set_bookings
+    add_bookings
     return "C'est fini !"
   end
 
   private
 
-  def set_url(property)
+  def set_url
     ap "je set l'url"
-    return URI("https://api.lodgify.com/v1/reservation?offset=0&limit=50&status=Booked&trash=false&propertyId=#{property.lodgify_id}")
+    @url = URI("https://api.lodgify.com/v2/reservations/bookings?page=#{@current_page_number}&size=50&includeCount=true&includeTransactions=false&includeExternal=false&includeQuoteDetails=true")
   end
 
-  def set_bookings(url)
-    ap "je set les bookings"
+  def set_json
+    ap "je set le json"
 
-    http = Net::HTTP.new(url.host, url.port)
+    http = Net::HTTP.new(@url.host, @url.port)
     http.use_ssl = true
 
-    request = Net::HTTP::Get.new(url)
+    request = Net::HTTP::Get.new(@url)
     request["Accept"] = 'text/plain'
-    request["X-ApiKey"] = ENV["LODGIFY_API_KEY"]
+    request["X-ApiKey"] = ENV.fetch("LODGIFY_API_KEY")
 
     response = http.request(request)
-    return JSON.parse(response.read_body)['items']
+    @json = JSON.parse(response.read_body)
   end
 
-  def create_bookings(api_bookings, property)
+  def set_page_number
+    ap "je set le nombre de pages"
+    count = @json["count"]
+    @page_number = (count / 50) + 1
+  end
+
+  def set_bookings
+    ap "je set les bookings"
+    @api_bookings = @json["items"]
+  end
+
+  def add_bookings
     ap "je crée les bookings"
-    api_bookings.each do |api_booking|
+    @api_bookings.each do |api_booking|
+      ap api_booking
       if Booking.where(lodgify_id: api_booking['id']).empty?
         ## Set the Api-Guest
         api_guest = api_booking['guest']
@@ -62,9 +77,7 @@ class BookingsInTheInboxService
     end
 
     return "Creating done !"
-
   end
-
 
   def create_guest(api_guest)
     if Guest.where(lodgify_id: api_guest['id']).empty?
