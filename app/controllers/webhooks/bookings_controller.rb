9ede4524a3a15fbf7e07a20263ded7956640f7be
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-05-26 13:05:04 +0200

    want to check want i receive

--- app/controllers/webhooks/bookings_controller.rb
+++ app/controllers/webhooks/bookings_controller.rb
@@ -1,4 +1,18 @@
 class Webhooks::BookingsController < ActionController::Base
   def create
+    p ""
+    p ""
+    p ""
+    p ""
+    p ""
+    p ""
+    ap "pour voir ce que je recois de la requete post du webhook"
+    ap params
+    p ""
+    p ""
+    p ""
+    p ""
+    p ""
+    p ""
   end
 end
