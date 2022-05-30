cc5e25b3605e15f276e58a4b775724382036f585
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-05-30 17:00:15 +0200

    toto

--- toto.rb
+++ toto.rb
@@ -1,5 +1,10 @@
+# def get_amount(caution)
+#   caution.split(' ').filter { |word| word.chars.include?("€") }.first.chars.delete_if { |c| c == "€" }.join.gsub(",", ".").to_f if caution.include?("€")
+# end
+
 def get_amount(caution)
-  caution.split(' ').filter { |word| word.chars.include?("€") }.first.chars.delete_if { |c| c == "€" }.join.gsub(",", ".").to_f
+  result = caution.split(' ').filter { |word| word[0..-2] == word[0..-2].to_i.to_s }.filter { |number| number.length >= 3}.first.to_i
+  result == 0 ? 1000 : result
 end
 
-p get_amount("Caution 2: 1000,82€ sous 5 jours")
+p get_amount("N/D")
