21d2216c7e28387614703443350b19e06711078a
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-05-30 23:30:27 +0200

    added the method to get the number of french

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,21 @@
 ap "welcome to the unusual seeds"
 
-def good_deposit
+def count_good_deposit
   bookings_nil = Booking.all.filter { |booking| booking.deposit == 1000 }
-  ap "#{bookings_nil.count} / #{Booking.count}"
+  ap "#{bookings_nil.count} good deposit on #{Booking.count}"
+end
+
+def count_french
+  frenchs = Booking.all.filter { |booking| booking.guest.country_code == "FR" || booking.guest.phone[0..1] == "06" || booking.guest.phone[0..1] == "07" }
+  ap "#{frenchs.count} french on #{Booking.count}"
 end
 
 def display_phones
   Booking.all.each { |booking| ap booking.guest.phone }
 end
 
-# good_deposit()
+count_good_deposit()
 
-display_phones()
+# display_phones()
+
+count_french()
