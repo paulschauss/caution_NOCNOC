f4665518bea18b3b6e6f55ce7faea4bb143cb745
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-05-30 23:10:26 +0200

    renamed the contact client service to before end

---
+++ app/services/contact_before_end_service.rb
@@ -0,0 +1,16 @@
+class ContactBeforeEndService
+  def call
+    Booking.all.each do |booking|
+      if booking.departure == Date.tomorrow
+        send_message(booking)
+      end
+    end
+    return "Messages sended to clients"
+  end
+
+  private
+
+  def send_message(booking)
+    ap "Send the message to #{booking.guest.name}"
+  end
+end
