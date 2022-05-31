8ed06b9e9bd90daf65771aa7310ebb736b766324
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-05-31 14:34:20 +0200

    twilio and from france method

--- app/services/contact_before_end_service.rb
+++ app/services/contact_before_end_service.rb
@@ -1,8 +1,13 @@
 class ContactBeforeEndService
+
+  def initialize
+    @client = Twilio::REST::Client.new(ENV.fetch('ACCOUNT_SID'), ENV.fetch('AUTH_TOKEN'))
+  end
+
   def call
     Booking.all.each do |booking|
       if booking.departure == Date.tomorrow
-        send_message(booking)
+        from_france?(booking) ? send_message_to_fr(booking) : send_message_to_en(booking)
       end
     end
     return "Messages sended to clients"
@@ -10,7 +15,23 @@ class ContactBeforeEndService
 
   private
 
-  def send_message(booking)
-    ap "Send the message to #{booking.guest.name}"
+  def from_france?(booking)
+    booking.guest.phone[0..2] == "+33"
   end
+
+  def send_message_to_fr(booking)
+    ap "Send the message to #{booking.guest.name}, FRENCH version"
+    message = @client.messages.create(
+      from: ENV.fetch('TWILIO_NUMBER'),
+      to: booking.guest.phone,
+      body: "Bonjour #{booking.guest.name}, nous vous remercions de votre confiance et de votre séjour chez #{booking.property.name}. Nous vous souhaitons une bonne journée. A bientôt !"
+    )
+    ap message
+  end
+
+  def send_message_to_en(booking)
+    ap "Send the message to #{booking.guest.name} ENGLISH version"
+
+  end
+
 end
