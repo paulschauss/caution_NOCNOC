ee6168664f08a3ced0d8963afd887fa50dcb916e
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-06-13 16:14:44 +0200

    refacto

--- app/services/one_day_before_checkout_service.rb
+++ app/services/one_day_before_checkout_service.rb
@@ -5,39 +5,42 @@ class OneDayBeforeCheckoutService
   end
 
   def call
-    # Booking.includes(:guest).where(departure: Date.tomorrow).each do |booking|
-    #   from_france?(booking.guest) ? send_message_to_fr(booking) : send_message_to_en(booking)
-    # end
+    Booking.includes(:guest).where(departure: Date.tomorrow).each do |booking|
+      @booking = booking
+      @guest = booking.guest
+      @property = booking.property
+      send_message()
+    end
 
-    # pour tester
-    booking = Booking.find_by(lodgify_id: 32279172)
-    send_message_to_fr(booking)
-
-    return "Messages sended to clients"
+    ## pour tester
+    # @booking = Booking.find_by(lodgify_id: 32279172)
+    # @guest = @booking.guest
+    # @property = @booking.property
+    # send_message()
   end
 
   private
 
-  def from_france?(guest)
-    guest.phone[0..2] == "+33"
+  def from_france?
+    @guest.phone[0..2] == "+33"
   end
 
-  def send_message_to_fr(booking)
-    ap "Send the message to #{booking.guest.name}, FRENCH version, url: #{@base_url}/#{booking.caution.id}"
-    message = @client.messages.create(
-      from: ENV.fetch('TWILIO_NUMBER'),
-      to: booking.guest.phone,
-      body: "Bonjour #{booking.guest.name}, nous vous remercions de votre confiance et de votre séjour chez #{booking.property.name}. Nous vous souhaitons une bonne journée. A bientôt !"
-    )
-  end
-
-  def send_message_to_en(booking)
-    ap "Send the message to #{booking.guest.name} ENGLISH version"
+  def send_message
+    ap "Send the message to #{@guest.name}, #{from_france?() ? "FRENCH" : "ENGLISH"} version"
     # message = @client.messages.create(
     #   from: ENV.fetch('TWILIO_NUMBER'),
-    #   to: booking.guest.phone,
-    #   body: "Hello #{booking.guest.name}, we are very pleased to have you at #{booking.property.name}. We hope you have a good day. See you soon !"
+    #   to: @guest.phone,
+    #   body: body()
     # )
   end
+
+  def body
+    if from_france?()
+      "Bonjour #{@guest.name}, nous vous remercions de votre confiance et de votre séjour chez #{@property.name}. Nous vous souhaitons une bonne journée. A bientôt !"
+    else
+      "Hello #{@guest.name}, we are very pleased to have you at #{@property.name}. We hope you have a good day. See you soon !"
+    end
+  end
+
 
 end
