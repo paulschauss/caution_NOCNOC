5b03adfe86605561631555310200712728c67a0b
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-06-13 15:58:50 +0200

    dependent destroy caution for booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :guest
   belongs_to :property
-  has_one :caution
+  has_one :caution, dependent: :destroy
 
   validates :lodgify_id, presence: true, uniqueness: true
 end
