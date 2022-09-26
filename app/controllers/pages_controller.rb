729fd5a583dccf12f5a2cff633b7fb0e72a32ef9
paul schauss <p+github@schauss.fr>
2022-09-26 17:06:45 +0200

    dashboard start

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @bookings_of_today = Booking.where("arrival = ?", Date.today)
   end
 end
