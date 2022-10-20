4aba6b32f8d6b8aab6019e5a452c8b60dc78b758
paul schauss <p+github@schauss.fr>
2022-10-20 15:24:47 +0200

    fix: rm gemfile.lock & bundle

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,8 @@
 Rails.configuration.stripe = {
-  publishable_key: Rails.env.production? ? ENV.fetch['STRIPE_PUBLIC_KEY'] : ENV.fetch('TEST_STRIPE_PUBLIC_KEY'),
-  secret_key:      Rails.env.production? ? ENV.fetch['STRIPE_SECRET_KEY'] : ENV.fetch('TEST_STRIPE_SECRET_KEY')
+  publishable_key:
+  Rails.env.production? ? ENV.fetch['STRIPE_PUBLIC_KEY'] : ENV.fetch('TEST_STRIPE_PUBLIC_KEY'),
+  secret_key:
+  Rails.env.production? ? ENV.fetch['STRIPE_SECRET_KEY'] : ENV.fetch('TEST_STRIPE_SECRET_KEY')
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
